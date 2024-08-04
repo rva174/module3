20f77b2bc54846c1d8dcbed63030d55a3073b8c6
rva174 <rva174@yandex.ru>
2024-08-04 23:10:51 +0500

    init

--- module_3_1.py
+++ module_3_1.py
@@ -1,31 +1,37 @@
 calls = 0
 
 def count_calls():
-    global calls
-
-    calls += 1
-
+    calls = 0
+    calls =  calls + 1
+pass
 def string_info(string):
-    string = ('Urban')
-    string_info = (len(string), [string.upper(), string.lower()])
-    print(string_info)
+
+    input(string)
+    string = 'Urban'
+
+    print(string = (len(string), string.upper(), string.lower()))
 
 
     count_calls()
 
+
     return string
-
-
+    string_info(string)
 
 def is_contains(string, list_to_search):
     string = string.lower()
     list_to_search = [value.lower() for value in list_to_search]
+    print(is_contains)
+    count_calls() == 'forward'
 
-    count_calls()
+
+
 
     return string in list_to_search
 
+
+
 #print(string_info('Capybara'))
-print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
-print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
-print(calls)
+#print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
+#print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+#print(calls)
--- module_3_2.py
+++ module_3_2.py
@@ -1,2 +1,55 @@
-print('Введите строку:'
-jjj
+calls = 0
+
+#count_calls(calls)
+#global calls
+
+calls = 0
+
+
+####
+
+#input(f"{string})
+string = 'GeLaTin'
+string_info = (len(string), string.upper(), string.lower())
+print(string_info)
+string1 = 'Urban'
+string_info = (len(string1), string1.upper(), string1.lower())
+print(string_info)
+
+#count_calls()
+
+print(calls)
+
+#return string
+#string_info(string)
+
+
+string = 's0kEr'
+list_to_search = ['forWard', 'goal', 'SparTAK']
+is_contains = (string, list_to_search)
+print(is_contains)
+
+string = string.lower()
+list_to_search = [value.lower() for value in list_to_search]
+print(is_contains)
+
+string == 'forward' # для сравнения
+
+string == 'horse'   # ля сравнения
+
+calls = calls + 1
+print(calls)
+
+#return string in list_to_searc
+
+
+
+
+
+#print(is_contains)
+#count_calls()
+
+
+#    return string in list_to_searc
+
+#is_contains(string, list_to_search)
