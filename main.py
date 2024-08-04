9612500a03fdee18f3d9e920c65383a85a5eaff7
rva174 <rva174@yandex.ru>
2024-08-04 14:51:58 +0500

    init

--- main.py
+++ main.py
@@ -1,1 +1,39 @@
-yyyy
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+print(len(numbers))
+print('Numbers: ',numbers)
+primes = []
+not_primes = []
+is_prime = True
+#i = 2
+for i in range(2, numbers[len(numbers)]):
+    k = 1
+    for j in range(2, i + 1):
+    if i % j == 0:
+is_prime = True:
+    primes.append(i)
+ #               k += 1
+    else:
+
+    not_primes.append(i)
+
+
+ #           if is_prime == False:
+  #              break
+
+
+
+print('Primes:', primes)
+print('Not Primes:',not_primes)
+
+
+
+ #           if is_prime == False:
+ #               break
+ #   if is_prime == True:
+
+
+
+
+
+
+
---
+++ module_3_1.py
@@ -0,0 +1,54 @@
+
+
+#calls = 0
+#def count_calls(func):
+
+
+
+#def string_info(list):
+
+string_info = 'Urban'
+string_info = (len(string_info), string_info.lower(), string_info.upper())
+print(string_info)
+
+
+
+
+
+#any(farm in is_contains for item in my_string)
+
+is_contains =  ('farm', ['horse', 'cow'])
+print(is_contains)
+
+bool = True
+
+for string in is_contains:
+    if string in is_contains:
+        if string == 'farm':
+            print(bool)
+            print('yes')
+        if string != 'farm':
+            bool = False
+            print(bool)
+            print('no')
+
+
+################################
+
+def is_contains(a, b):
+    bool = True
+
+    for string in is_contains:
+        if string in is_contains:
+            if string == 'farm':
+                print(bool)
+                print('yes')
+            if string != 'farm':
+                bool = False
+                print(bool)
+                print('no')
+
+
+
+
+
