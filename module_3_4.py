b67fc728c50fe55dd34344652075d9e8480ec081
rva174 <rva174@yandex.ru>
2024-08-12 23:32:44 +0500

    init

--- module_3_4.py
+++ module_3_4.py
@@ -1,4 +1,3 @@
-
 def single_root_words(root_words, *other_words):  #*args
 
     same_words = []
@@ -7,7 +6,7 @@ def single_root_words(root_words, *other_words):  #*args
         if (root_words.lower() in (item.lower()) or (item.lower()) in root_words.lower()):
             same_words.append(item)
 
-    print('result_1:', same_words)
+    print(same_words)
 
 
 result1 = single_root_words('rich','richiest', 'orichalcum', 'cheers', 'richies')
