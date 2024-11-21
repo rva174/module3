30e131e8231ed70a49338cee15691485062a72a5
rva174 <rva174@yandex.ru>
2024-11-21 20:14:16 +0500

    init

--- module_3_2.py
+++ module_3_2.py
@@ -1,16 +1,21 @@
 # Способы вызова функции
 def send_email(message, recipient, *, sender="univercity.help@gmail.com"):
 
-    if sender == 'univercity.help@gmail.com':
-        print(f'"Письмо успешно отправлено с адреса {sender} на адрес {recipient}"')
+
+    if (("@" or ".com" or ".ru" or ".net") not in sender or ("@" or ".com" or ".ru" or ".net") not in recipient):
+        print(f'"Невозможно отправить письмо с адреса {sender} на адрес {recipient}"')
+
+ #   if ("@" or ".com" or ".ru" or ".net") not in sender or ("@" or ".com" or ".ru" or ".net") not in recipient):
+ #       print(f'"Невозможно отправить письмо с адреса {sender} на адрес {recipient}"')
+
+#    if ("@" or (".com" and ".ru" and ".net")) not in recipient:
+ #       print(f'"Невозможно отправить письмо с адреса {sender} на адрес {recipient}"')
 
     elif recipient == sender:
         print(f'"Нельзя отправить письмо самому себе!"')
 
-
-    elif (("@" and (".com" or ".ru" or ".net")) not in recipient
-          and ("@" and (".com" or ".ru" or ".net")) not in sender):
-        print(f'"Невозможно отправить письмо с адреса {sender} на адрес {recipient}"')
+    elif sender == 'univercity.help@gmail.com':
+        print(f'"Письмо успешно отправлено с адреса {sender} на адрес {recipient}"')
 
     else:
         print(f'"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}"')
@@ -19,6 +24,6 @@ def send_email(message, recipient, *, sender="univercity.help@gmail.com"):
 send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
 send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
 send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
-send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
+send_email('Напоминаю самому себе о вебинаре',  'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
 
 
